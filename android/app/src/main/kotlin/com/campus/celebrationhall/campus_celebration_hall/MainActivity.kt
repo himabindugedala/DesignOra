3a66e5db4a3719a07ad262450a041eadd2556661
Lakshmi-create1 <dhanagorli6@gmail.com>
2026-09-15 10:16:38 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/campus/celebrationhall/campus_celebration_hall/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.campus.celebrationhall.campus_celebration_hall
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/campus_celebration_hall/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.campus_celebration_hall
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
